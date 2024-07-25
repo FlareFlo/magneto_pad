ab7604446ebdc2c8fc88ab070978e8c5f49b8210
FlareFlo <mail@flareflo.dev>
2024-07-25 17:43:40 +0200

    Add embassy

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,188 @@
 #![no_std]
 #![no_main]
 
-use rtic_monotonics::rp2040::prelude::*;
+use core::sync::atomic::{AtomicBool, Ordering};
 
-rp2040_timer_monotonic!(Mono);
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_futures::join::join;
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{Input, Pull};
+use embassy_rp::peripherals::USB;
+use embassy_rp::usb::{Driver, InterruptHandler};
+use embassy_usb::class::hid::{HidReaderWriter, ReportId, RequestHandler, State};
+use embassy_usb::control::OutResponse;
+use embassy_usb::{Builder, Config, Handler};
+use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
+use {defmt_rtt as _, panic_probe as _};
 
-#[rtic::app(device = rp_pico::hal::pac)]
-mod app {
-    use super::*;
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
 
-    use rp_pico::hal::{
-        clocks,
-        gpio::{
-            self,
-            bank0::{Gpio2, Gpio25, Gpio3},
-            FunctionSio, PullNone, PullUp, SioOutput,
-        },
-        pac,
-        sio::Sio,
-        watchdog::Watchdog,
-        I2C,
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    // Create the driver, from the HAL.
+    let driver = Driver::new(p.USB, Irqs);
+
+    // Create embassy-usb Config
+    let mut config = Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("HID keyboard example");
+    config.serial_number = Some("12345678");
+    config.max_power = 100;
+    config.max_packet_size_0 = 64;
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    // You can also add a Microsoft OS descriptor.
+    let mut msos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+    let mut request_handler = MyRequestHandler {};
+    let mut device_handler = MyDeviceHandler::new();
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut msos_descriptor,
+        &mut control_buf,
+    );
+
+    builder.handler(&mut device_handler);
+
+    // Create classes on the builder.
+    let config = embassy_usb::class::hid::Config {
+        report_descriptor: KeyboardReport::desc(),
+        request_handler: None,
+        poll_ms: 60,
+        max_packet_size: 64,
     };
-    use rp_pico::XOSC_CRYSTAL_FREQ;
+    let hid = HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut state, config);
 
-    use core::mem::MaybeUninit;
-    use defmt::*;
-    use defmt::panic;
-    use fugit::RateExtU32;
-    use panic_probe as _;
-    use defmt_rtt as _;
-    use embedded_hal::digital::OutputPin;
-    use embedded_hal::digital::StatefulOutputPin;
+    // Build the builder.
+    let mut usb = builder.build();
 
-    type I2CBus = I2C<
-        pac::I2C1,
-        (
-            gpio::Pin<Gpio2, gpio::FunctionI2C, PullUp>,
-            gpio::Pin<Gpio3, gpio::FunctionI2C, PullUp>,
-        ),
-    >;
+    // Run the USB device.
+    let usb_fut = usb.run();
 
-    #[shared]
-    struct Shared {}
+    // Set up the signal pin that will be used to trigger the keyboard.
+    let mut signal_pin = Input::new(p.PIN_16, Pull::None);
 
-    #[local]
-    struct Local {
-        led: gpio::Pin<Gpio25, FunctionSio<SioOutput>, PullNone>,
-    }
+    // Enable the schmitt trigger to slightly debounce.
+    signal_pin.set_schmitt(true);
 
-    #[init(local=[
-        // Task local initialized resources are static
-        // Here we use MaybeUninit to allow for initialization in init()
-        // This enables its usage in driver initialization
-        i2c_ctx: MaybeUninit<I2CBus> = MaybeUninit::uninit()
-    ])]
-    fn init(mut ctx: init::Context) -> (Shared, Local) {
-        // Configure the clocks, watchdog - The default is to generate a 125 MHz system clock
-        Mono::start(ctx.device.TIMER, &mut ctx.device.RESETS); // default rp2040 clock-rate is 125MHz
-        let mut watchdog = Watchdog::new(ctx.device.WATCHDOG);
-        let clocks = clocks::init_clocks_and_plls(
-            XOSC_CRYSTAL_FREQ,
-            ctx.device.XOSC,
-            ctx.device.CLOCKS,
-            ctx.device.PLL_SYS,
-            ctx.device.PLL_USB,
-            &mut ctx.device.RESETS,
-            &mut watchdog,
-        )
-        .ok()
-        .unwrap();
+    let (reader, mut writer) = hid.split();
 
-        // Init LED pin
-        let sio = Sio::new(ctx.device.SIO);
-        let gpioa = rp_pico::Pins::new(
-            ctx.device.IO_BANK0,
-            ctx.device.PADS_BANK0,
-            sio.gpio_bank0,
-            &mut ctx.device.RESETS,
-        );
-        let mut led = gpioa
-            .led
-            .into_pull_type::<PullNone>()
-            .into_push_pull_output();
-        led.set_high().unwrap();
-
-        // // Init I2C pins
-        // let sda_pin: gpio::Pin<_, gpio::FunctionI2C, _> = gpioa
-        //     .gpio2
-        //     .into_pull_up_disabled()
-        //     .reconfigure();
-        // let scl_pin: gpio::Pin<_, gpio::FunctionI2C, _> = gpioa
-        //     .gpio3
-        //     .into_pull_up_disabled()
-        //     .reconfigure();
-        //
-        // // Init I2C itself, using MaybeUninit to overwrite the previously
-        // // uninitialized i2c_ctx variable without dropping its value
-        // // (i2c_ctx definined in init local resources above)
-        // let i2c_tmp: &'static mut _ = ctx.local.i2c_ctx.write(I2C::i2c1(
-        //     ctx.device.I2C1,
-        //     sda_pin,
-        //     scl_pin,
-        //     100.kHz(),
-        //     &mut ctx.device.RESETS,
-        //     &clocks.system_clock,
-        // ));
-
-        // Spawn heartbeat task
-        heartbeat::spawn().unwrap();
-
-        // Return resources and timer
-        (Shared {}, Local { led })
-    }
-
-    #[task(local = [led])]
-    async fn heartbeat(ctx: heartbeat::Context) {
-        // Loop forever.
-        //
-        // It is important to remember that tasks that loop
-        // forever should have an `await` somewhere in that loop.
-        //
-        // Without the await, the task will never yield back to
-        // the async executor, which means that no other lower or
-        // equal  priority task will be able to run.
+    // Do stuff with the class!
+    let in_fut = async {
         loop {
-            // Flicker the built-in LED
-            ctx.local.led.toggle().unwrap();
-            // info!("balls");
+            info!("Waiting for HIGH on pin 16");
+            signal_pin.wait_for_high().await;
+            info!("HIGH DETECTED");
+            // Create a report with the A key pressed. (no shift modifier)
+            let report = KeyboardReport {
+                keycodes: [4, 0, 0, 0, 0, 0],
+                leds: 0,
+                modifier: 0,
+                reserved: 0,
+            };
+            // Send the report.
+            match writer.write_serialize(&report).await {
+                Ok(()) => {}
+                Err(e) => warn!("Failed to send report: {:?}", e),
+            };
+            signal_pin.wait_for_low().await;
+            info!("LOW DETECTED");
+            let report = KeyboardReport {
+                keycodes: [0, 0, 0, 0, 0, 0],
+                leds: 0,
+                modifier: 0,
+                reserved: 0,
+            };
+            match writer.write_serialize(&report).await {
+                Ok(()) => {}
+                Err(e) => warn!("Failed to send report: {:?}", e),
+            };
+        }
+    };
 
-            // Congrats, you can use your i2c and have access to it here,
-            // now to do something with it!
+    let out_fut = async {
+        reader.run(false, &mut request_handler).await;
+    };
 
-            // Delay for 1 second
-            Mono::delay(1000.millis()).await;
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join(usb_fut, join(in_fut, out_fut)).await;
+}
+
+struct MyRequestHandler {}
+
+impl RequestHandler for MyRequestHandler {
+    fn get_report(&mut self, id: ReportId, _buf: &mut [u8]) -> Option<usize> {
+        info!("Get report for {:?}", id);
+        None
+    }
+
+    fn set_report(&mut self, id: ReportId, data: &[u8]) -> OutResponse {
+        info!("Set report for {:?}: {=[u8]}", id, data);
+        OutResponse::Accepted
+    }
+
+    fn set_idle_ms(&mut self, id: Option<ReportId>, dur: u32) {
+        info!("Set idle rate for {:?} to {:?}", id, dur);
+    }
+
+    fn get_idle_ms(&mut self, id: Option<ReportId>) -> Option<u32> {
+        info!("Get idle rate for {:?}", id);
+        None
+    }
+}
+
+struct MyDeviceHandler {
+    configured: AtomicBool,
+}
+
+impl MyDeviceHandler {
+    fn new() -> Self {
+        MyDeviceHandler {
+            configured: AtomicBool::new(false),
+        }
+    }
+}
+
+impl Handler for MyDeviceHandler {
+    fn enabled(&mut self, enabled: bool) {
+        self.configured.store(false, Ordering::Relaxed);
+        if enabled {
+            info!("Device enabled");
+        } else {
+            info!("Device disabled");
+        }
+    }
+
+    fn reset(&mut self) {
+        self.configured.store(false, Ordering::Relaxed);
+        info!("Bus reset, the Vbus current limit is 100mA");
+    }
+
+    fn addressed(&mut self, addr: u8) {
+        self.configured.store(false, Ordering::Relaxed);
+        info!("USB address set to: {}", addr);
+    }
+
+    fn configured(&mut self, configured: bool) {
+        self.configured.store(configured, Ordering::Relaxed);
+        if configured {
+            info!("Device configured, it may now draw up to the configured current limit from Vbus.")
+        } else {
+            info!("Device is no longer configured, the Vbus current limit is 100mA.");
         }
     }
 }
