1891ac5c47697474997d0785ec5341770ae2a938
FlareFlo <mail@flareflo.dev>
2024-07-27 15:03:45 +0200

    Automatically set permissions for USB device

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,7 @@
 mod kb_handle;
 
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
 use std::time::Duration;
 use rusb::{Device, GlobalContext};
 use shared::message::Message;
@@ -8,6 +10,12 @@ use shared::VENDOR_ID;
 fn main() {
     let device = get_keyboard().unwrap();
     println!("Found keyboard! Bus {:03} Device {:03}", device.bus_number(), device.address());
+
+    sudo::escalate_if_needed().unwrap();
+    let f = fs::File::open(format!("/dev/bus/usb/{:03}/{:03}", device.bus_number(), device.address())).unwrap();
+    let mut perms = f.metadata().unwrap().permissions();
+    perms.set_mode(0o0666);
+    f.set_permissions(perms).unwrap();
 
     let kb = device.open().unwrap();
     kb.claim_interface(2).unwrap();
