176ec362c72da91c0112c83079e05cfb4d594ae7
zUnixorn <dev@jonascrull.de>
2024-09-21 19:27:09 +0200

    impl wasd with keyberon

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -20,12 +20,9 @@ use embassy_time::Timer;
 use keyberon::action::Action;
 use keyberon::key_code::{KbHidReport, KeyCode};
 use keyberon::layout::{Event, Layout};
-use usbd_hid::descriptor::KeyboardReport;
-use crate::hid::{HidChannel, run_hid};
 use crate::usb::setup_usb;
 
 mod usb;
-// mod hid;
 mod util;
 mod constants;
 mod hid;
@@ -61,9 +58,7 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(config);
 
     let channel: Channel<NoopRawMutex, KbHidReport, 10> = Channel::new();
-    // let channel: HidChannel = Channel::new();
     let sender = channel.sender();
-    // let hid = run_hid(p.PA0.degrade(), p.EXTI0.degrade(), channel.sender());
     let usb = setup_usb(p.USB_OTG_FS, channel.receiver(), p.PA12, p.PA11);
 
     let flash = Flash::new(p.FLASH, Irqs);
@@ -92,42 +87,72 @@ async fn main(_spawner: Spawner) {
             ]
         ]);
 
-        let mut keys = [KeyState {
-            min: u16::MAX,
-            max: u16::MIN,
-            pressed: false,
-            config: KeyConfig::Threshold(1500),
-        }; 4];
+        let mut keys = AnalogueMatrix::new([KeyConfig::Threshold(1500); 4]);
 
-        let mut a = [0; 4];
-        let mut r = [false; 4];
+        let mut previous_report = None;
 
         loop {
-            for i in 0..4 {
-                a[i] = reader.sample(i);
-                keys[i].update(a[i]);
-
-                let _ = layout.event(if keys[i].pressed {
-                    Event::Press(0, i as u8)
+            for (x, pressed) in keys.get(&mut reader) {
+                let _ = layout.event(if pressed {
+                    Event::Press(0, x as u8)
                 } else {
-                    Event::Release(0, i as u8)
+                    Event::Release(0, x as u8)
                 });
             }
+            //
+            // let _ = layout.tick();
 
-            let report: KbHidReport = layout.tick().collect();
+            if let Some(ref prev_report) = previous_report {
+                let report: KbHidReport = layout.tick().collect();
 
-            sender.send(report).await;
+                if *prev_report != report {
+                    previous_report = Some(report.clone());
+                    sender.send(report).await;
+                }
+            } else {
+                let report: KbHidReport = layout.tick().collect();
+                previous_report = Some(report);
+            }
 
             // info!("k1: {:?}, k2: {:?}, k3: {:?}, k4: {:?}", keys[0].pressed, keys[1].pressed, keys[2].pressed, keys[3].pressed);
 
-            Timer::after_micros(10).await;
-            // Timer::after_millis(1).await;
+            Timer::after_nanos(100).await;
         }
     };
 
-    // usb.await;
-
     join(usb, scanner).await;
+}
+
+struct AnalogueMatrix<const SIZE: usize> {
+    keys: [KeyState; SIZE]
+}
+
+impl<const SIZE: usize> AnalogueMatrix<SIZE> {
+    fn new(keys: [KeyConfig; SIZE]) -> Self {
+        Self {
+            keys: keys.map(|c| KeyState {
+                min: u16::MAX,
+                max: u16::MIN,
+                pressed: false,
+                changed: false,
+                // ADC max
+                last: [4096; SMA_WINDOW],
+                config: c,
+            })
+        }
+    }
+
+    fn get<'a>(&'a mut self, reader: &'a mut AnalogueReader) -> impl Iterator<Item = (usize, bool)> + 'a  {
+        for i in 0..SIZE {
+            let value = reader.sample(i);
+            self.keys[i].update(value);
+        }
+
+        self.keys.iter()
+            .enumerate()
+            .filter(|k| k.1.changed)
+            .map(|k| (k.0, k.1.pressed))
+    }
 }
 
 struct AnalogueReader<const AMOUNT: usize = 1> {
@@ -183,6 +208,8 @@ enum  KeyConfig {
     RappidTrigger(u16),
 }
 
+const SMA_WINDOW: usize = 8;
+
 // TODO convert to distance
 #[derive(Copy, Clone)]
 struct KeyState {
@@ -190,6 +217,8 @@ struct KeyState {
     pub max: u16,
     pressed: bool,
     config: KeyConfig,
+    changed: bool,
+    last: [u16; SMA_WINDOW]
 }
 
 impl KeyState {
@@ -202,8 +231,23 @@ impl KeyState {
             self.min = value;
         }
 
+        // update sma window
+        self.last.rotate_left(1);
+        self.last[SMA_WINDOW - 1] = value;
+
+        let avg = self.last.iter().sum::<u16>() / (SMA_WINDOW as u16);
+
         match self.config {
-            KeyConfig::Threshold(v) => self.pressed = value < v,
+            KeyConfig::Threshold(v) => {
+                let pressed = avg < v;
+
+                if pressed != self.pressed {
+                    self.changed = true;
+                    self.pressed = pressed;
+                } else {
+                    self.changed = false;
+                }
+            },
             KeyConfig::RappidTrigger(_) => todo!(),
         }
     }
